44f85335e9dfe20691cb436ab2789ef8eafa0d2a
Moises Mendoza <91902400+MoisesMendozza@users.noreply.github.com>
2021-10-04 16:51:52 -0500

    Desencriptado básico en ASCII

---
+++ ASCIIProgram(Translator)/ASCIIProgram(Translator).cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int c1, c2, c3, c4, c5;
+    cout << "Enter the ASCII message: ";
+    cin >> c1 >> c2 >> c3 >> c4 >> c5;
+    cout << "This is your message   : " << char(c1)
+        << char(c2) << char(c3) << char(c4) << char(c5);
+    
+
+
+    system("pause>0");  
+}
